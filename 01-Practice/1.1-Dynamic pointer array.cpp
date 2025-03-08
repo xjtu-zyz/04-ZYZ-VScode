fc7af44fac794c40020efe7136483e562d7768b6
xjtu-zyz <18737405312@163.com>
2025-03-08 15:34:47 +0800

    second commit

--- 01-Practice/1.1-Dynamic pointer array.cpp
+++ 01-Practice/1.1-Dynamic pointer array.cpp
@@ -3,18 +3,18 @@ using namespace std;
 int main() {
 	int x;
 	cin >> x;
-    int **p=new int*[x]();   //题目要求使用指向指针的指针变量！
+    int **p=nullptr;   //题目要求使用指向指针的指针变量！
     int sum=0;
     for(int i=0;i<x;i++)
     {
-    	p[i]=new int; 
+    	p[i]=new int;        //p=new int *[x];
         cin>>*p[i];
         sum+=*p[i];   
     }
 	cout<<sum<<endl;
 	 for (int i=0;i<x;i++)
 	  {
-        delete p[i]; 
+        if(p[i])delete p[i]; 
       }
 	if(p) delete[]p;
     //system("pause");
--- 01-Practice/1.3-const+pointer.cpp
+++ 01-Practice/1.3-const+pointer.cpp
@@ -11,11 +11,15 @@ int main(){
 	const char *q=new char[5]{'d','e','f',' '};
 	cout<<q<<strlen(q)+1<<endl;  //strlen不包括'\0'，所以+1
 	
-  
-	
+  //char *const q=new char[5]{'d','e','f'};  //这样定义q，q就不能改变了
+  //cout<<q<<strlen(q)+2<<endl;
+
 	char x[2];
 	gets(x);
     strcpy((char*)q,(char*)p); //属于强制转换类型，实现字符串内容的改变
+
+	//strcpy(q,p);
+	//cin>>q[strlen(q)];
     strcat((char*)q,x);
 
   
