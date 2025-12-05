3f1f8747ac00f34715ec03789d8ccf1a4c915a1a
LAF____ <LAF____@noreply.gitcode.com>
2025-12-05 13:25:16 +0800

    MCM-Exercise-A

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-import matplotlib
-print(matplotlib.matplotlib_fname())
+from dotenv import load_dotenv
+import os
+load_dotenv() # 加载 .env 文件中的环境变量
+api_key = os.getenv("SERP_API_KEY")
+if not api_key:
+   raise ValueError("未设置 API_KEY 环境变量")
+print(f"API Key: {api_key}")
